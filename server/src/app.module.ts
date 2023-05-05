78302456c7122771b50f830da0146e6331d82f50
Ayush Shashank <ayush.shashank@accenture.com>
2023-05-06 04:30:44 +0530

    Config fixes

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,23 +1,49 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { EmployeeModule } from './employee/employee.module';
+import { AdvertiserModule } from './advertiser/advertiser.module';
+import { AddressModule } from './address/address.module';
+import { AdvertismentModule } from './advertisment/advertisment.module';
+import { BudgetModule } from './budget/budget.module';
+import { CompanyModule } from './company/company.module';
+import { PlatformModule } from './platform/platform.module';
+import { ProductModule } from './product/product.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'admanagement',
-    entities: [],
-    synchronize: false,
-    entityPrefix: 'tbl_',
-    autoLoadEntities: true,
-  }), UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'AdManagement',
+      entities: [],
+      synchronize: false,
+      entityPrefix: 'tbl_',
+      autoLoadEntities: true,
+    }),
+    UsersModule,
+    EmployeeModule,
+    AdvertiserModule,
+    AddressModule,
+    AdvertismentModule,
+    BudgetModule,
+    CompanyModule,
+    PlatformModule,
+    ProductModule,
+    ReportModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      enableDebugMessages: true,
+      validationError: {
+        target: true,
+        value: true,
+      },
+      stopAtFirstError: true,
+    }),
+  );
+  swaggerSetup(app);
   await app.listen(3000);
 }
 bootstrap();
+
+function swaggerSetup(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Ad Management')
+    .setDescription('The Ad Management API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
