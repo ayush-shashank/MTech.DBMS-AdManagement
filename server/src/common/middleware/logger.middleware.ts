65c2f907185b7b2059ea9f6c26ac7c54b173557b
Ayush Shashank <ayush.shashank@accenture.com>
2023-05-07 05:14:44 +0530

    updated logger

--- server/src/common/middleware/logger.middleware.ts
+++ server/src/common/middleware/logger.middleware.ts
@@ -1,13 +1,23 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-    use(req: Request, res: Response, next: NextFunction) {
-        console.log(`${req.method}: ${req.path}`);
-        res.on("finish", () => {
-            console.log(` => ${res.statusCode} - ${res.statusMessage}`);
-        })
-        next();
-    }
+  use(req: Request, res: Response, next: NextFunction) {
+    Logger.log(req.path, req.method);
+    res.on('finish', () => {
+      const message = res.statusMessage;
+      const code = res.statusCode.toString();
+      if (res.statusCode >= 500) Logger.error(message, code);
+      else if (res.statusCode >= 400) Logger.warn(message, code);
+      else if (res.statusCode >= 300) Logger.debug(message, code);
+      else if (res.statusCode >= 200) Logger.verbose(message, code);
+      else Logger.log(message, code);
+      //   Logger.error(
+      //     `${res.statusCode} - ${res.statusMessage}`,
+      //     `${req.method} ${req.path}`,
+      //   );
+    });
+    next();
+  }
 }
